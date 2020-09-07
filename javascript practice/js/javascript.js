14e069171ba9b976b76df1b822ea4e26fedc779a
강선구 <sungu1206@naver.com>
2020-09-08 06:55:08 +0900

    20.09.08

--- javascript practice/js/javascript.js
+++ javascript practice/js/javascript.js
@@ -1,1 +1,9 @@
-
+function test(){
+  var i = document.getElementsByClassName('nav')
+  var j = i[0].style.display;
+  if(j == 'block') {
+    i[0].style.display = 'none';
+  }else {
+    i[0].style.display = 'block';
+  }
+}
