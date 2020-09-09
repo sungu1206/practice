38f6fe91228a8c3b212b846e231a9b62ecdee320
강선구 <sungu1206@naver.com>
2020-09-09 21:04:26 +0900

    20.09.09

--- javascript practice/js/javascript.js
+++ javascript practice/js/javascript.js
@@ -1,9 +1,19 @@
-function test(){
-  var i = document.getElementsByClassName('nav')
-  var j = i[0].style.display;
-  if(j == 'block') {
-    i[0].style.display = 'none';
-  }else {
-    i[0].style.display = 'block';
+// function switchSideMenu() {
+// 	var temp = document.getElementById("sidemenu").style.left;
+// 	temp = eval(temp.replace(/[^0-9-]/g, ""));
+// 	if(temp > 0) {
+// 		document.getElementById("sidemenu").style.left = "-90px";
+// 		document.getElementById("sidemenu").getElementsByTagName("span")[0].innerHTML = "▶ click me!";
+// 	} else {
+// 		document.getElementById("sidemenu").style.left = "50px";
+// 		document.getElementById("sidemenu").getElementsByTagName("span")[0].innerHTML = "◀";
+// 	}
+// }
+function sliderMenu(){
+  var i = document.getElementsByClassName('slider');
+  if(i[0].style.visibility == 'visible'){
+    i[0].style.visibility = 'hidden'; 
+  }else{
+    i[0].style.visibility = 'visible';
   }
 }
---
+++ project3/javascript/javascript.js
@@ -0,0 +1,8 @@
+function dropDown() {
+  var i = document.getElementsByClassName('dropdown');
+  if(i[0].style.display == 'none'){
+    i[0].style.display = 'block';
+  }else {
+    i[0].style.display = 'none';
+  }
+};
