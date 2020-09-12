31ae7b5ed5e1e984a592bcb27754fb0588fa4c23
강선구 <sungu1206@naver.com>
2020-09-13 00:13:44 +0900

    20.09.12

--- SliderMenu(css , js)/js/javascript.js
+++ SliderMenu(css , js)/js/javascript.js
@@ -1,6 +1,5 @@
 function openNav() {
-  document.getElementsByClassName("menu")[0].style.width = '250px';
-}
+  document.getElementsByClassName('menu')[0].style.width = '250px';
 
 function closeNav() {
   document.getElementsByClassName('menu')[0].style.width = '0px';
---
+++ TransformMenu(css, js)/js/javascript.js
@@ -0,0 +1,3 @@
+function myFunction() {
+  document.getElementsByClassName('container')[0].classList.toggle('barstyle');
+}
