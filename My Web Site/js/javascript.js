b20aa6e0c9cc7702884e8cf361cfdadbdb13d7ce
강선구 <sungu1206@naver.com>
2020-09-19 21:15:23 +0900

    20.09.19

--- My Web Site/js/javascript.js
+++ My Web Site/js/javascript.js
@@ -3,19 +3,9 @@ document.getElementsByClassName('nav-bar')[0].addEventListener('click', function
   document.getElementsByClassName('nav-bar')[0].classList.toggle('trans');
   var a = document.getElementsByClassName('nav');
   var b = document.getElementsByClassName('nav-bar');
-    if(a[0].style.width === ''){
+    if(a[0].style.width == ''){
       a[0].style.width = '304px';
     }else {
       a[0].style.width = '';
     }
   });
-document.getElementsByClassName('dropdown-1')[0].addEventListener('click',function dropdownClick(){
-  var c = document.getElementsByClassName('dropdown-list-1');
-      c[0].classList.toggle('drop');
-      c[0].style.backgroundColor = '#f0f0f0';
-});
-document.getElementsByClassName('dropdown-2')[0].addEventListener('click',function dropdownClick(){
-  var d = document.getElementsByClassName('dropdown-list-2');
-      d[0].classList.toggle('drop');
-      d[0].style.backgroundColor = '#f0f0f0';
-});
