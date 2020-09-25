3ceab55730d71a1a7065209b3ded4a4d8027bc28
강선구 <sungu1206@naver.com>
2020-09-25 19:15:44 +0900

    20.09.25

--- My Web Site/js/javascript.js
+++ My Web Site/js/javascript.js
@@ -1,11 +1,3 @@
-document.getElementsByClassName('dropdown')[0].addEventListener('click' , function clickDrop(){
-  var a = document.getElementsByClassName('dropdown');
-  var b = document.getElementsByClassName('dropdown_list');
-  if(b[0].style.display == ''){
-    a[0].style.backgroundColor = 'gray';
-    b[0].style.display = 'block';
-  }else{
-    a[0].style.backgroundColor = '';
-    b[0].style.display = '';
-  }
-});
+window.onload = function loadingBar(){
+  // var i = document.getElementsByClassName('loading');
+  for()
