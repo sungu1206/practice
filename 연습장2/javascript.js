f1d3597627a83a132f1cdc4d0b1c20a98ea5f486
강선구 <sungu1206@naver.com>
2020-10-21 21:04:32 +0900

    20.10.21

--- 연습장2/javascript.js
+++ 연습장2/javascript.js
@@ -1,7 +1,26 @@
-// 함수 = 1. 선언! , 2. 호출!
-
-function add(x , y){
-    let sum = x * y;
-    return sum; 
+// 함수
+function plus(num1, num2) {
+    return num1 + num2;
 }
-console.log(add(1 , 2));
+
+function minus(num1, num2) {
+    return num1 / num2;
+}
+
+function surprise(operator){
+    const result = operator(2, 3);
+    document.write(result);
+}
+surprise(minus);
+
+
+ 
+//
+let counter = 0;
+const btn1 = document.querySelector('.btn1');
+const sum = document.querySelector('.sum');
+btn1.addEventListener('click', () => {
+    console.log(counter++);
+    sum.style.width = `${counter}px`;
+    sum.style.height = `${counter}px`;
+});
