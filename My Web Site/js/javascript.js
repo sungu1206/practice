77ae47da5fa1c0cd169c3596cc21914722a209dd
강선구 <sungu1206@naver.com>
2020-10-27 21:24:37 +0900

    20.10.27

--- My Web Site/js/javascript.js
+++ My Web Site/js/javascript.js
@@ -1,8 +1,1 @@
-window.onload = function(){
-  let i = document.getElementsByClassName('loading');
-  if(i[0].style.width === '1000px'){
-    i[0].style.width = '0px';
-  }else {
-    i[0].style.width = '1000px';
-  }
-};
+ 
