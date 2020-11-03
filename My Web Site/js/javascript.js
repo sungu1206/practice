9bcb2912473a781a75ad3412ac4e31afbdeb019c
강선구 <sungu1206@naver.com>
2020-11-03 18:17:16 +0900

    20.11.03

--- My Web Site/js/javascript.js
+++ My Web Site/js/javascript.js
@@ -1,25 +1,25 @@
-const sideBtn = document.querySelector('.sideBtn');
-const sideBtnIcon = document.querySelector('.sideBtn i');
-const sideMenu = document.querySelector('.sideMenu');
-const sideWrap = document.querySelector('.wrap');
+const sideBtn = document.querySelector('.btn');
+const sideBtnIcon = document.querySelector('.btn i');
+const sideMenu = document.querySelector('menu');
+const sideWrap = document.querySelector('.main_wraper');
 sideBtn.addEventListener('click', () => {
     if(sideMenu.style.width === '0px'){
         sideMenu.style.width = '250px';
         sideBtn.style.left = '250px';
-        sideBtn.style.transition = '0.2s';
-        sideMenu.style.transition = '0.2s';
+        sideBtn.style.transition = '0.3s';
+        sideMenu.style.transition = '0.3s';
         sideBtnIcon.style.transform = 'rotate(1turn)';
-        sideBtnIcon.style.transition = '0.2s';
-        sideWrap.style.marginLeft = '0px';
-        sideWrap.style.transition = '0.2s';
+        sideBtnIcon.style.transition = '0.3s';
+        sideWrap.style.marginLeft = '115px';
+        sideWrap.style.transition = '0.3s';
     }else{
         sideMenu.style.width = '0px';
         sideBtn.style.left = '0px';
-        sideBtn.style.transition = '0.2s';
-        sideMenu.style.transition = '0.2s';
+        sideBtn.style.transition = '0.3s';
+        sideMenu.style.transition = '0.3s';
         sideBtnIcon.style.transform = 'rotate(0.5turn)';
-        sideBtnIcon.style.transition = '0.2s';
-        sideWrap.style.marginLeft = '-250px';
-        sideWrap.style.transition = '0.2s';
+        sideBtnIcon.style.transition = '0.3s';
+        sideWrap.style.marginLeft = '0px';
+        sideWrap.style.transition = '0.3s';
     }
 });
