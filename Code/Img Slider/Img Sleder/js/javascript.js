805762ff76abf10adfa14ccc963660f037600278
강선구 <sungu1206@naver.com>
2020-11-09 20:15:02 +0900

    20.11.09

--- Code/Img Slider/Img Sleder/js/javascript.js
+++ Code/Img Slider/Img Sleder/js/javascript.js
@@ -8,6 +8,8 @@ let counter = 0;
 
 firstItem.classList.add('active');
 
+
+
 prevBtn.addEventListener('click' , () => {
 
     console.log(++counter , items[counter]);
@@ -17,7 +19,7 @@ prevBtn.addEventListener('click' , () => {
         items[counter-1].classList.remove('active');
     }if(counter === items.length){
         firstItem.classList.add('active');
-        items[counter-1].classList.remove('active');
+        items[counter-1].classList.remove('active'); 
         counter = 0;
     }
 
@@ -35,4 +37,17 @@ backBtn.addEventListener('click' , () => {
     }
 });
 
- 
+
+const radio = document.querySelector('.radio input ');
+const radioFirst = document.querySelector('.radio:first-child');
+const radioClass = document.getElementsByClassName('radio')[0];
+const radicoTag = '<input type="radio" name="radio" class="radio_btn"></input>';
+
+
+     function loop(){
+         for(let i = 0; i < items.length; i++){
+            radioClass.innerHTML += `${radicoTag}`;
+         }
+     }
+     loop();
+     console.log(radio.childNodes)
