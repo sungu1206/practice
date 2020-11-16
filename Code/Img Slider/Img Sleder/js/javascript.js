c20010771664601b18b9f98442a7cda0d897f517
강선구 <sungu1206@naver.com>
2020-11-16 22:29:23 +0900

    20.11.16

--- Code/Img Slider/Img Sleder/js/javascript.js
+++ Code/Img Slider/Img Sleder/js/javascript.js
@@ -1,68 +1,70 @@
-const items = document.querySelectorAll('.item');
-const firstItem = document.querySelector('.item:first-child');
+// const items = document.querySelectorAll('.item');
+// const firstItem = document.querySelector('.item:first-child');
 
-const backBtn = document.querySelector('.back_btn');
-const prevBtn = document.querySelector('.prev_btn');
+// const backBtn = document.querySelector('.back_btn');
+// const prevBtn = document.querySelector('.prev_btn');
 
-let counter = 0;
+// let counter = 0;
 
-firstItem.classList.add('active');
-
-
-prevBtn.addEventListener('click' , () => {
-
-    console.log(++counter , items[counter]);
-
-    if(items[counter]){
-        items[counter].classList.add('active');
-        items[counter-1].classList.remove('active');
-
-    }if(counter === items.length){
-        firstItem.classList.add('active');
-        items[counter-1].classList.remove('active'); 
-        counter = 0;
-    }
-
-});
-
-backBtn.addEventListener('click' , () => {
-    console.log(--counter , items[counter]);
-    if(items[counter]){
-        items[counter].classList.add('active');
-        items[counter+1].classList.remove('active');
-
-    }if(counter <= -1){
-        firstItem.classList.remove('active');
-        counter = items.length-1;
-        items[counter].classList.add('active');
-    }
-});
+// firstItem.classList.add('active');
 
 
+// prevBtn.addEventListener('click' , () => {
 
-const radio = document.getElementsByClassName('radio')[0];
-const createTag = document.createElement('input');
+//     console.log(++counter , items[counter]);
 
-for(let i = 0; i < items.length; i++){
-    createTag.type = 'radio';
-    createTag.name = 'radio_btn';
-    createTag.value =  '';
-    radio.appendChild(createTag.cloneNode(true));
-}
+//     if(items[counter]){
+//         items[counter].classList.add('active');
+//         items[counter-1].classList.remove('active');
+
+//     }if(counter === items.length){
+//         firstItem.classList.add('active');
+//         items[counter-1].classList.remove('active'); 
+//         counter = 0;
+//     }
+
+// });
+
+// backBtn.addEventListener('click' , () => {
+//     console.log(--counter , items[counter]);
+//     if(items[counter]){
+//         items[counter].classList.add('active');
+//         items[counter+1].classList.remove('active');
+
+//     }if(counter <= -1){
+//         firstItem.classList.remove('active');
+//         counter = items.length-1;
+//         items[counter].classList.add('active');
+//     }
+// });
 
 
 
+// const radio = document.getElementsByClassName('radio')[0];
+// const createTag = document.createElement('input');
+
+// for(let i = 0; i < items.length; i++){
+//     createTag.type = 'radio';
+//     createTag.name = 'radio_btn';
+//     createTag.value =  '';
+//     radio.appendChild(createTag.cloneNode(true));
+// }
 
 
-const radioSelector = document.querySelectorAll('input');
-let j = 0;
 
-function looploop(){
-    for(j = 0; j < items.length; j++){
-        radioSelector[j].addEventListener('click', () => {
-            console.log(items.values);
-        })
-    }
-    return looploop;
-}
-console.log(looploop());
+
+
+// const radioSelector = document.querySelectorAll('input');
+// let j = 0;
+
+// function looploop(){
+//     for(j = 0; j < items.length; j++){       
+//         radioSelector[j].addEventListener('click', () => {
+            
+//         });
+//     }
+
+// }
+// console.log(looploop());
+
+ 
