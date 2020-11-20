d9fd5a35de1a467c598c8db289686ca9e436cac5
강선구 <sungu1206@naver.com>
2020-11-20 22:55:55 +0900

    20.11.20

--- Code/Img Slider/Img Sleder/js/javascript.js
+++ Code/Img Slider/Img Sleder/js/javascript.js
@@ -1,44 +1,42 @@
 // const items = document.querySelectorAll('.item');
 // const firstItem = document.querySelector('.item:first-child');
 
-// const backBtn = document.querySelector('.back_btn');
-// const prevBtn = document.querySelector('.prev_btn');
+const backBtn = document.querySelector('.back_btn');
+const prevBtn = document.querySelector('.prev_btn');
 
-// let counter = 0;
+let counter = 0;
 
 // firstItem.classList.add('active');
 
 
-// prevBtn.addEventListener('click' , () => {
+prevBtn.addEventListener('click' , () => {
 
-//     console.log(++counter , items[counter]);
+    console.log(++counter , items[counter]);
 
-//     if(items[counter]){
-//         items[counter].classList.add('active');
-//         items[counter-1].classList.remove('active');
+    if(items[counter]){
+        items[counter].classList.add('active');
+        items[counter-1].classList.remove('active');
 
-//     }if(counter === items.length){
-//         firstItem.classList.add('active');
-//         items[counter-1].classList.remove('active'); 
-//         counter = 0;
-//     }
+    }if(counter === items.length){
+        firstItem.classList.add('active');
+        items[counter-1].classList.remove('active'); 
+        counter = 0;
+    }
 
-// });
+});
 
-// backBtn.addEventListener('click' , () => {
-//     console.log(--counter , items[counter]);
-//     if(items[counter]){
-//         items[counter].classList.add('active');
-//         items[counter+1].classList.remove('active');
+backBtn.addEventListener('click' , () => {
+    console.log(--counter , items[counter]);
+    if(items[counter]){
+        items[counter].classList.add('active');
+        items[counter+1].classList.remove('active');
 
-//     }if(counter <= -1){
-//         firstItem.classList.remove('active');
-//         counter = items.length-1;
-//         items[counter].classList.add('active');
-//     }
-// });
-
-
+    }if(counter <= -1){
+        firstItem.classList.remove('active');
+        counter = items.length-1;
+        items[counter].classList.add('active');
+    }
+});
 
 
 
@@ -47,18 +45,18 @@ const sliderDiv = document.querySelector('.slider');
 const items = document.querySelectorAll('.item');
 
 const radioDiv = document.querySelector('.radio');
-const input = document.getElementsByTagName('input');
+const input = document.querySelectorAll('input');
 
 const createRadio = document.createElement('input');
 createRadio.type = 'radio';
-createRadio.className = 'radio_btn';
 createRadio.name = 'radio';
 const firstItem = document.querySelector('.item:first-child');
-function looploop(a , b , c , d , e){
-    d.style
+
+function looploop(){
+    firstItem.classList.add('active');
     for(let i = 0; i < items.length; i++){
-        b.appendChild(c.cloneNode(true));
+        radioDiv.appendChild(createRadio.cloneNode(true));
+        
     }
-    return a , b , c , d , e;
 }
-looploop(items , radioDiv , createRadio , firstItem , input);
+console.log(looploop());
