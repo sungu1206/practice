b7c5b8b30f4bd0fe6609c4582671dd1f1eca965d
강선구 <sungu1206@naver.com>
2021-01-28 21:34:12 +0900

    21.01.28

--- Code/Test/Test5/js/javascript.js
+++ Code/Test/Test5/js/javascript.js
@@ -1,49 +1,36 @@
 'use strict';
+// buttons(input)
+const prev = document.querySelector(".prev");
+const next = document.querySelector(".next");
 
-//Counter
-let counter = 0;
+const plus = document.querySelector(".plus");
+const minus = document.querySelector(".minus");
 
-//Buttons(input)
-const prevBtn = document.querySelector(".prev_btn");
-const nextBtn = document.querySelector(".next_btn");
-
-const minusBtn = document.querySelector(".minus_btn");
-const plusBtn = document.querySelector(".plus_btn");
-
-//Slider(div)
-const slider = document.querySelector(".slider");
-
-//Gallery(ul)
+// gallery(ul)
 const gallery = document.querySelector(".gallery");
 
-//Item(li)
-const item = document.getElementsByClassName("item");
+// item(li)
+const item = document.querySelector(".item");
+//cerate(li)
+const list = document.createElement("li");
 
-//cerate
-const createItem = document.createElement("li");
+//counter
+let counter = 0;
+const items = document.querySelectorAll(".item")[counter];
 
-function plusEventer(a, b) {
+
+function plusEventer(a, b, c) {
     a.addEventListener("click", () => {
-        console.log(++counter);
-        createItem.setAttribute("class", `item${counter}`);
-        const result1 = b(gallery, createItem);
+        console.log(counter++);
+        const result1 = b(list, gallery);
         console.log(result1);
     });
-}
-plusEventer(plusBtn, cloneElemenet);
+};
+const realPlusEventer = plusEventer(plus, setting);
 
-function cloneElemenet(a, b) {
-    return a.appendChild(b.cloneNode(false));
+function setting(a, b) {
+    a.setAttribute("class", "item");
+    b.appendChild(a.cloneNode(false));
+    return a, b;
 }
-
-function minusEventer(a, b) {
-    a.addEventListener("click", () => {
-        const result3 = b(gallery, item);
-    });
-}
-minusEventer(minusBtn, removeElement);
-
-function removeElement(a, b) {
-  return a.removeChild(b);
-}
-
+ 
