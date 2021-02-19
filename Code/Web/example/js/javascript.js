8400224fd6abeb287f2f212d071058c13b5c2705
강선구 <sungu1206@naver.com>
2021-02-19 20:03:52 +0900

    21.02.19

--- Code/Web/example/js/javascript.js
+++ Code/Web/example/js/javascript.js
@@ -1,2 +1,19 @@
 "use strict";
 
+const colck = document.querySelector(".clock");
+const mainClock = document.querySelector(".main_clock");
+
+function realTime(num) {
+    const time = new Date();
+    const hour = time.getHours();
+    const minutes = time.getMinutes();
+    colck.innerHTML = `${hour}:${minutes}`;
+}
+function init() {
+    setInterval(realTime, 1000);
+}
+init();
+
+
+    
+    
