9fa0ed83034511372bf0cc7f53e2d4fb4891567e
강선구 <sungu1206@naver.com>
2021-02-24 02:23:18 +0900

    21.02.24

--- Code/Test/Test6/js/javascript.js
+++ Code/Test/Test6/js/javascript.js
@@ -1,2 +1,23 @@
 "use strict";
 
+const clock1 = document.querySelector(".clock1");
+const clock2 = document.querySelector(".clock2");
+
+function time() {
+    const date = new Date();
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const day = date.getDay();
+}
+
+function innerTime() {
+    clock1.innerHTML = `${hours}:${minutes}`;
+    clock2.innerHTML = `${hours}:${minutes}.${day}`;
+}
+
+function operator(a, b) {
+    a + b;
+    return a, b;
+}
+const result = operator(time, innerTime);
+console.log(result)
