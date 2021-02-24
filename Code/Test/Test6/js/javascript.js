0db6518fb0e846d7ac357a01d1658b396269be30
강선구 <sungu1206@naver.com>
2021-02-24 23:51:35 +0900

    21.02.24

--- Code/Test/Test6/js/javascript.js
+++ Code/Test/Test6/js/javascript.js
@@ -2,22 +2,33 @@
 
 const clock1 = document.querySelector(".clock1");
 const clock2 = document.querySelector(".clock2");
+const clock3 = document.querySelector(".day");
 
 function time() {
     const date = new Date();
     const hours = date.getHours();
     const minutes = date.getMinutes();
     const day = date.getDay();
-}
-
-function innerTime() {
+    const month = date.getMonth();
+    const days = date.getDate();
     clock1.innerHTML = `${hours}:${minutes}`;
-    clock2.innerHTML = `${hours}:${minutes}.${day}`;
+    clock2.innerHTML = `${hours}:${minutes}`;
+    clock3.innerHTML = `${month}:${days}`;
 }
 
-function operator(a, b) {
-    a + b;
-    return a, b;
+function init(sum) {
+    setInterval(sum(), 1000);
 }
-const result = operator(time, innerTime);
-console.log(result)
+init(time);
+
+const months = {
+    monday : '월요일',
+    tuseday : "화요일",
+    wednesday :"수요일",
+    thursday : "목요일",
+    friday : "금요일",
+    saturday : "토요일",
+    sunday : "일요일"
+};
+
+const {monday, tusday, wednesday, thursday, friday, saturday, sunday} = day;
