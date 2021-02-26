01144d2366380402b2029af37a8371adf8b1e42f
강선구 <sungu1206@naver.com>
2021-02-27 02:13:14 +0900

    21.02.27

--- Code/Test/Test6/js/clock.js
+++ Code/Test/Test6/js/clock.js
@@ -3,13 +3,23 @@
 class Clock {
     constructor() {
         this.date = new Date();
-        this.date.getMonth();
-        this.date.getDate();
-        this.date.getDay();
-        this.date.getHours();
-        this.
-    }
+        const month = this.date.getMonth();
+        const days = this.date.getDate();
+        const day = this.date.getDay();
+        const hours = this.date.getHours();
+        const minutes = this.date.getMinutes();
+        this.clock1 = document.querySelector(".clock1");
+        this.clock2 = document.querySelector(".clock2");
 
+        this.clock1.innerHTML = `${hours < 10 ? "0" + hours : hours }:${minutes < 10 ? "0" + minutes : minutes }`;
+        this.clock2.innerHTML = `${hours < 10 ? "0" + hours : hours }<br>${minutes < 10 ? "0" + minutes : minutes }`;
+
+        if(hours > 12) {
+            hours-= 12;
+            this.clock1.innerHTML = `${hours < 10 ? "0" + hours : hours }:${minutes < 10 ? "0" + minutes : minutes }`;
+        }
+    }
 }
 
 const clock = new Clock();
+ 
