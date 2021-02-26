788d3511baa65e8179e1c24247715db309fdc8c3
강선구 <sungu1206@naver.com>
2021-02-26 23:43:12 +0900

    21.02.26

---
+++ Code/Test/Test6/js/clock.js
@@ -0,0 +1,15 @@
+"use strict";
+
+class Clock {
+    constructor() {
+        this.date = new Date();
+        this.date.getMonth();
+        this.date.getDate();
+        this.date.getDay();
+        this.date.getHours();
+        this.
+    }
+
+}
+
+const clock = new Clock();
