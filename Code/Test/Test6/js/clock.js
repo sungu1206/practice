d9879159aae64701eadb0530fa4979de4bcc614e
강선구 <sungu1206@naver.com>
2021-03-04 02:48:55 +0900

    21.03.04

--- Code/Test/Test6/js/clock.js
+++ Code/Test/Test6/js/clock.js
@@ -2,14 +2,27 @@
 
 class Clock {
     constructor() {
-        this.date = new Date();
         this.clock1 = document.querySelector(".clock1");
         this.clock2 = document.querySelector(".clock2");
         this.clock3 = document.querySelector(".clock3");
+        
+        setInterval(() => {
+            const date = new Date();
+            const hh = date.getHours();
+            const mm = date.getMinutes();
+            const ss = date.getSeconds();
+            const month = date.getMonth() + 1;
+            const today = date.getDate();
+            const days = date.getDay() - 1;
+ 
+            const day = ["월요일", "화요일", "수요일", "목요일", "금요일", "토요일", "일요일"];
 
-        const hh = this.date.getHours();
-        const mm = this.date.getMinutes();
+            this.clock1.innerHTML = `${ hh < 10 ? "0" + hh : hh > 12 ? hh - 12 : hh }:${mm < 10 ? "0" + mm : mm }:${ss < 10 ? "0" + ss : ss }`;
+            this.clock2.innerHTML = `${ hh < 10 ? "0" + hh : hh > 12 ? hh - 12 : hh }<br>${mm < 10 ? "0" + mm : mm }`;
+            this.clock3.innerHTML = `${month}월 ${today}일 ${day[days]}`;
+
+        }, 1000);
     }
 }
 
-const clock = new Clock();
+const clock = new Clock(); 
