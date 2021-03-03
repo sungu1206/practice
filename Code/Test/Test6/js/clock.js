f961efc04a4419aba0fbf7a958d6675384d83033
강선구 <sungu1206@naver.com>
2021-03-03 23:50:05 +0900

    21.03.03

--- Code/Test/Test6/js/clock.js
+++ Code/Test/Test6/js/clock.js
@@ -2,7 +2,13 @@
 
 class Clock {
     constructor() {
-        
+        this.date = new Date();
+        this.clock1 = document.querySelector(".clock1");
+        this.clock2 = document.querySelector(".clock2");
+        this.clock3 = document.querySelector(".clock3");
+
+        const hh = this.date.getHours();
+        const mm = this.date.getMinutes();
     }
 }
 
