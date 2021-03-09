c1f36943b537d9d500b5795a0b405838c1ad5f5a
강선구 <sungu1206@naver.com>
2021-03-10 04:17:36 +0900

    21.03.10[C

--- Code/Test/Test6/js/javascript.js
+++ Code/Test/Test6/js/javascript.js
@@ -1,7 +1,11 @@
 "use strict";
 
-const item1 = document.querySelector(".item1");
-const item2 = document.querySelector(".item2");
-const item3 = document.querySelector(".item3");
+const nav = document.querySelector("nav");
 
-function 
+function overlap() {
+    nav.addEventListener("click", () => {
+        nav.classList.toggle("barstyle");
+    })
+}
+overlap();
+
