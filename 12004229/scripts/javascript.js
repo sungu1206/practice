5646cbcf237dcaeb17d0021f7a20dbba2592b75e
강선구 <sungu1206@naver.com>
2021-03-16 03:15:07 +0900

    21.03.16

--- 12004229/scripts/javascript.js
+++ 12004229/scripts/javascript.js
@@ -1,3 +1,9 @@
-$(".main-menu").mouseenter(function() {
-   $(".sub-menu").css("display", "flex");
-});
+//$(document).ready(function() {
+//    $("nav > .main-menu > li").mouseenter(function() {
+//        $(".sub-menu").stop().slideDown(200);
+//    })
+//    
+//    $("nav > .main-menu > li").mouseleave(function() {
+//    $(".sub-menu").stop().slideUp(200);
+//    })
+//});
