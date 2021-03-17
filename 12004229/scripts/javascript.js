ff75602d72a103ff07fd085fe70f69020825f0c6
강선구 <sungu1206@naver.com>
2021-03-17 23:21:33 +0900

    21.03.17

--- 12004229/scripts/javascript.js
+++ 12004229/scripts/javascript.js
@@ -1,9 +1,24 @@
-//$(document).ready(function() {
-//    $("nav > .main-menu > li").mouseenter(function() {
-//        $(".sub-menu").stop().slideDown(200);
-//    })
-//    
-//    $("nav > .main-menu > li").mouseleave(function() {
-//    $(".sub-menu").stop().slideUp(200);
-//    })
-//});
+$(document).ready(function() {
+    $("nav > .main-menu > li").mouseenter(function() {
+        $(".sub-menu").stop().slideDown(200);
+    });
+    
+    $("nav > .main-menu > li").mouseleave(function() {
+        $(".sub-menu").stop().slideUp(200);
+    });
+    
+    let counter = 0;
+    
+    setInterval(function() {
+        counter++;
+        console.log(counter);
+        
+        if(counter === $(".slide").children().length) {
+            counter = 0;
+        }else {
+            
+        }
+    }, 3000);
+    console.log($(".item").width())
+});
+
