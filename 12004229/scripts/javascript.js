f42a515b60a4ef84981d1100b3bfb82c992ea7a5
강선구 <sungu1206@naver.com>
2021-03-23 04:41:23 +0900

    21.03.23

--- 12004229/scripts/javascript.js
+++ 12004229/scripts/javascript.js
@@ -1,35 +1,28 @@
-$(document).ready(function() { 
-    $(".main-menu > li").mouseenter(function() {
-        $(".sub-menu").stop().slideDown(200); 
-    });
-    $(".main-menu > li").mouseleave(function() {
-        $(".sub-menu").stop().slideUp(200);
-    });
+$(document).ready(function() {
+    //nav
+        $("nav > ul > li").mouseenter(function() {
+           $(".sub-menu").stop().slideDown(200);
+        });
+        $("nav > ul > li").mouseleave(function() {
+           $(".sub-menu").stop().slideUp(200);
+        });
     
-    let counter = 0;
-    
+    //slide
+        let counter = 0;
     setInterval(function() {
-        counter++;
-        console.log(counter);
-        if(counter === 3) {
-            counter = 0;
-            $(".slide").css("transition", "none");
-            $(".slide").css("transform", "translateY("+(-300 * counter)+"px)");
-        }else{
-            $(".slide").css("transition", "0.4s");
-            $(".slide").css("transform", "translateY("+(-300 * counter)+"px)");
-        }
-    }, 3000);
-    
-        const tabBtn = $(".notice-menu a");
-        const tabCont = $(".container > ul");
+        const slide = $(".slider > .slide > .item");
+        const slideImg = $(".slider > .slide > .item > img").height();
+        const sliderImgs = $(".slider > .slide > .item > img").length;
         
-    $(".notice-menu > a").click(function() {
-        const target = $(this);
-        const index = target.index();
-        tabBtn.removeClass("active");
-        target.addClass("active");
-        tabCont.css("display", "none");
-        tabCont.eq(index).css("display", "flex");
-    })
-});
+        counter++;
+        
+        if(counter === sliderImgs) {
+            counter = 0;
+            $(slide).css("transition", "none");
+            $(slide).css("transform", "translateY("+(-slideImg * counter)+"px)");
+        }else{
+            $(slide).css("transition", "0.4s");
+            $(slide).css("transform", "translateY("+(-slideImg * counter)+"px)");
+        }
+    }, 3000)
+})
