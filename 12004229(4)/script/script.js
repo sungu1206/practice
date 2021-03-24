818e92692c369404c466aec4bca2c2055a772518
강선구 <sungu1206@naver.com>
2021-03-25 06:22:09 +0900

    21.03.25

---
+++ 12004229(4)/script/script.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+    // nav
+    $(".sub-menu-wrap").hide();
+    $(".main-menu > li").mouseenter(function() {
+       $(".sub-menu-wrap").stop().slideDown(300); 
+    });
+    $(".sub-menu").mouseleave(function() {
+       $(".sub-menu-wrap").stop().slideUp(300);
+    });
+    
+    // slide
+    setInterval(function() {
+        $(".slide a:first-child").fadeOut()
+        .next("a").fadeIn()
+        .end().appendTo(".slide");
+    }, 3000);
+    
+    //modal
+    $(".modal").hide();
+    $(".sub-notice > li:first").click(function() {
+        $(".modal").show();
+    });
+    $(".btn > button").click(function() {
+       $(".modal").hide();
+    });
+});
